2a144d11223383a18932d5ceed7bfbb87f5aa76a
Shivram kashyap <kashyapshivram512@gmail.com>
2026-01-05 02:32:59 +0530

    assdasd

---
+++ server.js
@@ -0,0 +1,66 @@
+const express = require("express");
+const https = require("https");
+const fs = require("fs");
+const cors = require("cors");
+const { OAuth2Client } = require("google-auth-library");
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+// ✅ Health check
+app.get("/api/auth/authorize", (req, res) => {
+    console.log(req.body, "Health check");
+  res.send("Backend running ✅");
+});
+
+// 🔴 PUT YOUR GOOGLE WEB CLIENT ID HERE
+const GOOGLE_CLIENT_ID =
+  "317999867006-c1chqsld8au82q2ai256mtj63ugav98p.apps.googleusercontent.com";
+
+const client = new OAuth2Client(GOOGLE_CLIENT_ID);
+
+app.post("/auth/google", async (req, res) => {
+  try {
+    const { idToken } = req.body;
+
+    if (!idToken) {
+      return res.status(400).json({ message: "Missing idToken" });
+    }
+
+    const ticket = await client.verifyIdToken({
+      idToken,
+      audience: GOOGLE_CLIENT_ID,
+    });
+
+    const payload = ticket.getPayload();
+
+    res.json({
+      success: true,
+      user: {
+        googleId: payload.sub,
+        email: payload.email,
+        name: payload.name,
+        picture: payload.picture,
+      },
+    });
+  } catch (err) {
+    res.status(401).json({
+      success: false,
+      message: "Invalid Google token",
+    });
+  }
+});
+
+// const options = {
+//   key: fs.readFileSync("localhost+1-key.pem"),
+//   cert: fs.readFileSync("localhost+1.pem"),
+// };
+
+// https.createServer(options, app).listen(3000, () => {
+//   console.log("🔐 HTTPS backend running https://localhost:3000");
+// });
+
+app.listen(3000, () => {
+  console.log("Backend running http://localhost:3000");
+});
