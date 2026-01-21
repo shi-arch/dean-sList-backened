119cba724694c305f839fd9b886679dbf8910131
Shivram kashyap <kashyapshivram512@gmail.com>
2026-01-22 03:48:47 +0530

    assdasd

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ app.get("/api/auth/authorize", async (req, res) => {
     // console.log(code_challenge, state, "CODE CHALLENGE AND STATE");
     //const token = await exchangeCodeForToken(code_challenge);
     //res.redirect("exp://10.40.0.105:8081/api/auth/token");
-    return res.redirect("exp://10.151.188.119:8081/api/auth/callback?code=" + code);
+    return res.redirect(302, "exp://10.151.188.119:8081/api/auth/callback?code=" + code);
     //res.send('NNNNNNNNNNNNNN')
 });
 
