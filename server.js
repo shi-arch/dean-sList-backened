87b7f17485e0b031ff379686c80a424fe4dd9527
Shivram <84413738+shi-arch@users.noreply.github.com>
2026-01-21 15:53:29 +0530

    Update server.js

--- server.js
+++ server.js
@@ -21,8 +21,7 @@ app.get("/api/auth/authorize", async (req, res) => {
     // console.log(code_challenge, state, "CODE CHALLENGE AND STATE");
     //const token = await exchangeCodeForToken(code_challenge);
     //res.redirect("exp://10.40.0.105:8081/api/auth/token");
-    const redirectUrl = `exp://10.40.0.105:8081?token=asasasasasasasa`;
-    return res.redirect(302, redirectUrl);
+    return res.redirect(302, "exp://10.40.0.105:8081/api/auth/callback?code=" + code);
 });
 
 app.get("/", async (req, res) => {
