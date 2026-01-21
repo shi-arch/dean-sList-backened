dbd8738926faaee967096cd1490875765f2d3931
Shivram <84413738+shi-arch@users.noreply.github.com>
2026-01-21 15:40:21 +0530

    Add token parameter to redirect URL

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ app.get("/api/auth/authorize", async (req, res) => {
     // console.log(code_challenge, state, "CODE CHALLENGE AND STATE");
     //const token = await exchangeCodeForToken(code_challenge);
     //res.redirect("exp://10.40.0.105:8081/api/auth/token");
-    const redirectUrl = `exp://10.40.0.105:8081`;
+    const redirectUrl = `exp://10.40.0.105:8081?token=asasasasasasasa`;
     return res.redirect(302, redirectUrl);
 });
 
