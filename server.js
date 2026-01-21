48e0a3c40c4709f75652bbc7ec88fb883ec11149
Shivram kashyap <kashyapshivram512@gmail.com>
2026-01-21 13:03:12 +0530

    assdasd

--- server.js
+++ server.js
@@ -5,44 +5,15 @@ const cors = require("cors");
 const { OAuth2Client } = require("google-auth-library");
 const axios = require("axios");
 const qs = require("qs");
-//require("dotenv").config()
+require("dotenv").config()
 
 const app = express();
 app.use(cors());
 app.use(express.json());
- 
+const PORT = process.env.PORT
 // ✅ Health check
 
-async function exchangeCodeForToken(code) {
-  try {
-    const data = qs.stringify({
-      client_id: process.env.GOOGLE_CLIENT_ID,
-      client_secret: process.env.GOOGLE_CLIENT_SECRET,
-      code,
-      grant_type: "authorization_code",
-      redirect_uri: process.env.GOOGLE_REDIRECT_URI,
-    });
-    console.log(data, "DATA>>>>>>>>>>");
-    const response = await axios.post(
-      "https://oauth2.googleapis.com/token",
-      data,
-      {
-        headers: {
-          "Content-Type": "application/x-www-form-urlencoded",
-        },
-      }
-    );
-    console.log(response, "response>>>>>>>>>>");
 
-    return response.data;
-  } catch (error) {
-    console.error("Google token exchange failed:", {
-      status: error.response?.status,
-      data: error.response?.data,
-    });
-    throw error;
-  }
-}
 app.get("/api/auth/authorize", async (req, res) => {
     // console.log(req.body, "Health check");
     // console.log(req.query, "Query parameters");
@@ -60,43 +31,6 @@ app.get("/", async (req, res) => {
 
 
 // 🔴 PUT YOUR GOOGLE WEB CLIENT ID HERE
-const GOOGLE_CLIENT_ID =
-  "317999867006-c1chqsld8au82q2ai256mtj63ugav98p.apps.googleusercontent.com";
-
-const client = new OAuth2Client(GOOGLE_CLIENT_ID);
-
-
-app.post("/auth/google", async (req, res) => {
-  try {
-    const { idToken } = req.body;
-
-    if (!idToken) {
-      return res.status(400).json({ message: "Missing idToken" });
-    }
-
-    const ticket = await client.verifyIdToken({
-      idToken,
-      audience: GOOGLE_CLIENT_ID,
-    });
-
-    const payload = ticket.getPayload();
-
-    res.json({
-      success: true,
-      user: {
-        googleId: payload.sub,
-        email: payload.email,
-        name: payload.name,
-        picture: payload.picture,
-      },
-    });
-  } catch (err) {
-    res.status(401).json({
-      success: false,
-      message: "Invalid Google token",
-    });
-  }
-});
 
 // const options = {
 //   key: fs.readFileSync("localhost+1-key.pem"),
@@ -107,9 +41,9 @@ app.post("/auth/google", async (req, res) => {
 //   console.log("🔐 HTTPS backend running https://localhost:3000");
 // });
 //module.exports = app;
-app.listen(3000, () => {
+app.listen(PORT, () => {
   try {
-    console.log("🔓 HTTP backend running http://localhost:3000");
+    console.log("🔓 HTTP backend runninxxxxxg http://localhost:" + PORT);
   } catch (error) {
     console.error("Error starting server:", error);
   }
