7e2806d60e5a5e681e12616cb6117bbeac246549
Shivram kashyap <kashyapshivram512@gmail.com>
2026-01-28 22:18:31 +0530

    assdasd

--- server.js
+++ server.js
@@ -18,6 +18,42 @@ app.get("/api/auth/authorize", async (req, res) => {
     return res.redirect(302, LOCAL_REDIRECT_URI + "?code=" + req.query.code);
 });
 
+app.post('/payment-sheet', async (req, res) => {
+  // Use an existing Customer ID if this is a returning customer.
+  const amount = 1099;
+  const customer = await stripe.customers.create({"stripeAccount":'{{CONNECTED_ACCOUNT_ID}}'});
+  const customerSession = await stripe.customerSessions.create({
+    customer: customer.id,
+    components: {
+      mobile_payment_element: {
+        enabled: true,
+        features: {
+          payment_method_save: 'enabled',
+          payment_method_redisplay: 'enabled',
+          payment_method_remove: 'enabled'
+        }
+      },
+    },
+  });
+  const paymentIntent = await stripe.paymentIntents.create({
+    amount: 1099,
+    currency: 'gbp',
+    customer: customer.id,
+    // In the latest version of the API, specifying the `automatic_payment_methods` parameter
+    // is optional because Stripe enables its functionality by default.
+    automatic_payment_methods: {
+      enabled: true,
+    },
+  });
+
+  res.json({
+    paymentIntent: paymentIntent.client_secret,
+    customerSessionClientSecret: customerSession.client_secret,
+    customer: customer.id,
+    publishableKey: process.env.publishableKey,
+  });
+});
+
 app.get("/", async (req, res) => {
   res.send("Test >>>>>>");
 });
